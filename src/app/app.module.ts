b8d6a5ac03f5e97c6210a86255fad085d289a5b2
Henrique Muniz Cavalcanti <hmuniz@stj.jus.br>
2019-03-27 11:07:23 -0300

    commit photos 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+  @Input() description: string;
+  @Input() url: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.description);
+  }
+
+}
